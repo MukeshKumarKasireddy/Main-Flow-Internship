6862d3e93d96b4c658725d6f9acc82316159f03c
MukeshKumarKasireddy <127771369+MukeshKumarKasireddy@users.noreply.github.com>
2025-06-05 11:58:10 +0530

    Add files via upload

---
+++ LoginApp/src/main/java/com/mycompany/loginapp/LoginApp.java
@@ -0,0 +1,18 @@
+package com.mycompany.loginapp;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author kasir
+ */
+public class LoginApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
